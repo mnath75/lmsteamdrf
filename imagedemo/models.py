049e9932e3b12fe09a59176e5e77a371063d3637
mnath75 <mnath.snt@gmail.com>
2022-04-20 16:09:21 +0530

    message

--- imagedemo/models.py
+++ imagedemo/models.py
@@ -3,9 +3,7 @@ from django.db import models
 # Create your models here.
 class Image(models.Model):
    
-    image_text = models.TextField(db_column='image_text',default=None,null=True)
+    image_text = models.ImageField(upload_to = "images/",default="null")
     
-    def __str__(self):
-        return self.image_text
-
+   
   
